aaebb7c90f39659b3789503134cbfb66fecb04a1
孙楷 <704725181@qq.com>
2020-04-09 19:50:54 +1000

    update

--- app/src/main/java/com/example/mymoviememoir/SignUpActivity.java
+++ app/src/main/java/com/example/mymoviememoir/SignUpActivity.java
@@ -1,14 +1,29 @@
 package com.example.mymoviememoir;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private View birthDayInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        initViews();
+    }
+
+    private void initViews() {
+        birthDayInput = findViewById(R.id.birthDayInput);
+        birthDayInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(SignUpActivity.this).setView(R.layout.date_picker_layout).create().show();
+            }
+        });
     }
 }
